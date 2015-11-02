39df6d246320a016ceee97fb3c05c8f5ea0720fc
huynhnhu1502 <chauhuynhnhu1502@gmail.com>
2015-11-02 23:17:58 +0700

    Create project

---
+++ WEB_QL_LUONG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WEB_QL_LUONG.Startup))]
+namespace WEB_QL_LUONG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
