1bc4f0a2f5723916630233e714bbaa8f18ff27d1
Adam <adamcheng1947@gmail.com>
2015-11-28 21:45:12 +0700

    fsdfsdfsd

---
+++ 3Layer/BIZ/BIZ_LoaiDonVi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Layer.BIZ
+{
+    public class BIZ_LoaiDonVi
+    {
+        LoaiDonVi _loaidonvi = new LoaiDonVi();
+        DAL.DAL_LoaiDonVi _dalloaidv = new DAL.DAL_LoaiDonVi();
+        public bool TimKiem(LoaiDonVi loaidonvi)
+        {
+            return _dalloaidv.TimKiem(loaidonvi);
+        }
+        public bool ThemLoai(LoaiDonVi loaidonvi)
+        {
+            return _dalloaidv.ThemLoai(loaidonvi);
+        }
+        public bool SuaLoai(LoaiDonVi loaidv)
+        {
+            return _dalloaidv.SuaLoai(loaidv);
+        }
+    }
+}
---
+++ 3Layer/DAL/DAL_LoaiDonVi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Layer.DAL
+{
+    class DAL_LoaiDonVi
+    {
+        QuanLyLuongEntities1 _db = new QuanLyLuongEntities1();
+        LoaiDonVi _loaidonvi = new LoaiDonVi();
+        public bool TimKiem(LoaiDonVi loaidonvi)
+        {
+            var search = _db.LoaiDonVis.Where(a => a.MaLoai == loaidonvi.MaLoai || a.TenLoai == loaidonvi.TenLoai);
+            if (search != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool ThemLoai(LoaiDonVi loaidonvi)
+        {
+            try
+            {
+                var them = new LoaiDonVi { MaLoai = loaidonvi.MaLoai, TenLoai = loaidonvi.TenLoai, MoTa = loaidonvi.MoTa };
+                _db.LoaiDonVis.Add(them);
+                _db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool SuaLoai(LoaiDonVi loaidv)
+        {
+            try
+            {
+                var sua = _db.LoaiDonVis.FirstOrDefault(a => a.MaLoai == loaidv.MaLoai);
+                if (sua != null)
+                {
+                    sua.TenLoai = loaidv.TenLoai;
+                    sua.MoTa = loaidv.MoTa;
+                    _db.SaveChanges();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
