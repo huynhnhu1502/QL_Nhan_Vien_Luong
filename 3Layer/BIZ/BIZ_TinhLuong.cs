5421afc52f4e5b760bdb5d8f34c94d9d93ada45c
Adam <adamcheng1947@gmail.com>
2015-12-12 21:58:41 +0700

    cập nhật 3 lớp

--- 3Layer/BIZ/BIZ_TinhLuong.cs
+++ 3Layer/BIZ/BIZ_TinhLuong.cs
@@ -14,5 +14,17 @@ namespace _3Layer.BIZ
         {
             return _dalTinhLuong.TinhLuong(Manv);
         }
+        public List<LuongThucTe> ThongKeLuong(string MaDonVi, string thang, string nam)
+        {
+            return _dalTinhLuong.ThongKeLuong(MaDonVi, thang, nam);
+        }
+        public List<NhanVien> XuatThucHienTinhLuong(string MaNV)
+        {
+            return _dalTinhLuong.XuatThucHienTinhLuong(MaNV);
+        }
+        public List<NhanVien> XuatTinhLuong(string Hoten)
+        {
+            return _dalTinhLuong.XuatTinhLuong(Hoten);
+        }
     }
 }
